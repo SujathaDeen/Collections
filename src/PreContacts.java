e00bb4a5a93427e3eba2773f66e6d84d81791cd6
Sujatha <sujatha.deen@anywhere.co>
2021-04-15 16:38:46 +0530

    First commit

---
+++ src/PreContacts.java
@@ -0,0 +1,130 @@
+import collections.*;
+import java.util.*;
+import java.util.logging.Logger;
+import java.util.Scanner;
+
+
+class PreContacts implements Comparator<ContactDetails> {
+    Logger log=Logger.getLogger(Contacts.class.getName());
+    Scanner input=new Scanner(System.in);
+
+    void start(){
+        log.info("Contacts List");
+        while(true){
+            log.info("Do it");
+            log.info("\n1.ContactList \n2.Create Contact \n3.Search \n4.Call \n5.Delete \n6.Exit");
+            log.info("Enter to perform :");
+            int choice=input.nextInt();
+            switch (choice){
+                case 1:
+                    contactList();
+                    break;
+                case 2:
+                    savingOneContact();
+                    break;
+                case 3:
+                    searchContact();
+                    break;
+                case 4:
+                    callContact();
+                    break;
+                case 5:
+                    deleteContact();
+                    break;
+                case 6:
+                    System.exit(1);
+                    break;
+                default:
+                    log.info("Enter Between 1 to 6 ");
+                    break;
+            }
+        }
+    }
+
+    void savingOneContact(){
+        ContactDetails newContact=new ContactDetails();
+        log.info("Enter the Name : ");
+        String name=input.nextLine();
+        newContact.setName(name);
+        log.info("Enter the Mobile Number :");
+        int phoneNo=input.nextInt();
+        newContact.setPhoneNo(phoneNo);
+        log.info("Enter the Email : ");
+        String email=input.nextLine();
+        newContact.setEmail(email);
+        log.info("Enter the Notes : ");
+        String notes=input.nextLine();
+        newContact.setNotes(notes);
+        savingContacts(newContact);
+    }
+
+    List<ContactDetails> contactList=new ArrayList();
+    void savingContacts(ContactDetails newContact){
+        contactList.add(newContact);
+    }
+
+    void contactList(){
+        if(contactList.isEmpty()){
+            log.info("No contacts available");
+        }
+        else{
+            for(ContactDetails contacts:contactList){
+                log.info("\nName : "+contacts.getName()+"\nPhone No : "+contacts.getPhoneNo()+"\nEmail : "+contacts.getEmail()+"\nNotes : "+contacts.getNotes());
+            }
+        }
+    }
+
+    void searchContact(){
+        boolean flag=true;
+        log.info("Enter Phone No for Search : ");
+        int phoneNo=input.nextInt();
+        for(ContactDetails contacts:contactList){
+            if(contacts.getPhoneNo()==phoneNo){
+                log.info("\nSearched Contact");
+                log.info("\nName : "+contacts.getName()+"\nPhone No : "+contacts.getPhoneNo()+"\nEmail : "+contacts.getEmail()+"\nNotes : "+contacts.getNotes());
+                flag=false;
+            }
+        }
+        if(flag){
+            log.info("No such Records");
+        }
+
+    }
+
+    void callContact(){
+        boolean flag=true;
+        log.info("Enter Phone No for Call : ");
+        int phoneNo=input.nextInt();
+        for(ContactDetails contacts:contactList){
+            if(contacts.getPhoneNo()==phoneNo){
+                log.info("\n======================\n        Calling       \nName : "+contacts.getName()+"\nPhone No : "+contacts.getPhoneNo()+"\n======================");
+                flag=false;
+            }
+        }
+        if(flag){
+            log.info("No such Records");
+        }
+    }
+
+    void deleteContact(){
+        boolean flag=true;
+        log.info("Enter Phone No for Delete : ");
+        int phoneNo=input.nextInt();
+        for(ContactDetails contacts:contactList){
+            if(contacts.getPhoneNo()==phoneNo){
+                contactList.remove(contacts);
+                log.info("Contact Deleted");
+                flag=false;
+                break;
+            }
+        }
+        if(flag){
+            log.info("No such Records");
+        }
+    }
+
+    @Override
+    public int compare(ContactDetails o1, ContactDetails o2) {
+        return 0;
+    }
+}
---
+++ src/collections/ContactDetails.java
@@ -0,0 +1,41 @@
+package collections;
+
+public class ContactDetails {
+    private String name;
+    private int phoneNo;
+    private String email;
+    private String notes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(int phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+}
